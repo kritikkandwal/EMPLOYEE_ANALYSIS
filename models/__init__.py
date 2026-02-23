0b5fb4c7ca5f8a1311c30c4afc83755e474e1f9a
Kritik <kandwalktitik400@gmail.com>
2026-02-23 17:07:37 +0530

    fixing issue for deployment

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,3 @@
 from .database import db
-from .user import User, ProductivityLog, Badge
-from .attendance import AttendanceLog
-from .productivity import ProductivityAnalyzer, ProductivityCalculator
 
-__all__ = [
-    "db",
-    "User",
-    "ProductivityLog",
-    "Badge",
-    "AttendanceLog",
-    "ProductivityAnalyzer",
-    "ProductivityCalculator",
-]
+__all__ = ["db"]
