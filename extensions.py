708515317c9c7ca40e6e3e968a8dc6cd8befae36
Kritik <kandwalktitik400@gmail.com>
2026-02-23 18:14:58 +0530

    Remove duplicate db instance

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
 # extensions.py
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_login import LoginManager
 
 # Initialize extensions here to avoid circular imports
-db = SQLAlchemy()
+
 login_manager = LoginManager()
